4abdc5e0fe411f996edb4c62b97e32c0996e507b
Antoine Caron <antoine.caron@zenika.com>
2018-02-19 14:33:03 +0100

    first commit

---
+++ example/travellerProblem.js
@@ -0,0 +1,53 @@
+const _ = require('lodash');
+const chalk = require('chalk');
+const distance = require('gps-distance');
+const geneticAlgorithm = require('../index');
+const points = require('./capitals.json');
+
+const showTravel = listOfPoints => listOfPoints.map(p => p.name);
+
+const fitness = listOfPoints => distance(listOfPoints.map(point => [point.lat, point.lon]));
+
+const mutation = array => {
+    const items = [...array];
+    const firstRandomIndex = Math.floor(Math.random()*items.length);
+    let secondRandomIndex = firstRandomIndex;
+    while(secondRandomIndex === firstRandomIndex) {
+        secondRandomIndex = Math.floor(Math.random()*items.length);
+    } 
+    const temp = items[secondRandomIndex];
+    items[secondRandomIndex]= items[firstRandomIndex];
+    items[firstRandomIndex] = temp;
+    return items;
+};
+
+const startPopulation = [];
+
+for(let i = 0; i< 1000; i++) {
+    startPopulation.push(_.shuffle(points));
+}
+
+const combine = (gene1, gene2) => {
+    const cut = Math.floor(Math.random() * gene1.length);
+    const child = [];
+
+    for(let i= 0; i< cut; i++) {
+        child.push(gene1[i]);
+    }
+
+    let j = 0;
+    while(child.length !== gene1.length) {
+        if(!child.includes(gene2[j])) {
+            child.push(gene2[j]);
+        }
+        j++;
+    }
+
+    return child;
+};
+
+console.log(chalk.red('Begin'));
+
+const {bestFound} = geneticAlgorithm(500, startPopulation, fitness, combine, mutation, 0.1);
+
+console.log(chalk.yellow('Best travel found', showTravel(bestFound.gene), '', bestFound.fitness));
---
+++ index.js
@@ -0,0 +1,52 @@
+const _ = require('lodash');
+
+module.exports = function (nbOfGenerations, startPopulation, fitness, combine, mutation, mutationFactor) {
+    const selectionSize = Math.ceil(startPopulation.length / 2);
+    const nbToMutate = Math.ceil(startPopulation.length * mutationFactor);
+    let currentPopulation = startPopulation;
+
+    let bestFound = {
+      gene: _.first(startPopulation),
+      fitness: fitness(_.first(startPopulation))
+    };
+
+    for(let i = 0; i< nbOfGenerations; i ++) {
+        const evalutatePopulation = currentPopulation.map(elem => ({
+            gene: elem,
+            fitness: fitness(elem),
+        }));
+
+        const sortedPopulation = _.orderBy(evalutatePopulation, elem => elem.fitness);
+
+        // We select the best from the generation
+        const selection = sortedPopulation.slice(0, selectionSize);
+
+        const bestFromGeneration = _.first(selection);
+        if (bestFromGeneration.fitness < bestFound.fitness) {
+            bestFound = bestFromGeneration
+        }
+
+        let childrens = [];
+
+        for(let j = 0; j< selection.length; j+=2) {
+            childrens.push(combine(selection[j].gene, selection[j+1].gene))
+            childrens.push(combine(selection[j].gene, selection[j+1].gene))
+            childrens.push(combine(selection[j].gene, selection[j+1].gene))
+            childrens.push(combine(selection[j].gene, selection[j+1].gene))
+        }
+
+        _.sampleSize(childrens, nbToMutate).forEach(elem => {
+            const mutatedGene = mutation(elem);
+
+            return {
+                gene: mutatedGene,
+                fitness: fitness(mutatedGene)
+            }
+        });
+
+
+        currentPopulation = childrens;
+    }
+
+    return { bestFound }
+};
